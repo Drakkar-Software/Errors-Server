f4be04836560cb33eadf07a534d06b06142b35d4
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-08-23 22:06:44 +0200

    update errors model

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ import (
 // Init initializes the echo routes and registers the middleware
 func Init(e *echo.Echo) {
 	e.Pre(middleware.RemoveTrailingSlash())
-	// allow a max request size of 17000 characters (allows ~100 depth stack traces)
-	e.Use(middleware.BodyLimitWithConfig(middleware.BodyLimitConfig{Limit: "17K"}))
+	// allow a max request size of 17000 characters (allows 100 errors with ~100 depth stack traces)
+	e.Use(middleware.BodyLimitWithConfig(middleware.BodyLimitConfig{Limit: "1700K"}))
 	route.Init(e)
 }
