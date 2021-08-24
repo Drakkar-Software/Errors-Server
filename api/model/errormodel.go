5ab7ea01acc16f65e5a1ab3bf9163f173dc9b1d6
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-08-24 23:20:53 +0200

    update ErrorData model

--- api/model/errormodel.go
+++ api/model/errormodel.go
@@ -7,11 +7,13 @@ import (
 
 // ErrorData stores usage info about a specific error
 type ErrorData struct {
-	Title      string    `json:"title,omitempty" bson:"title"`
-	Type       string    `json:"type,omitempty" bson:"type"`
-	StackTrace []string  `json:"stacktrace,omitempty" bson:"stacktrace"`
-	Timestamps []float64 `json:"timestamps,omitempty" bson:"timestamps"`
-	MetricsId  string    `json:"metricsid,omitempty" bson:"metricsId"`
+	Title          string    `json:"title,omitempty" bson:"title"`
+	Type           string    `json:"type,omitempty" bson:"type"`
+	StackTrace     []string  `json:"stacktrace,omitempty" bson:"stacktrace"`
+	FirstTimestamp float64   `json:"firsttimestamp,omitempty" bson:"firstTimestamp"`
+	LastTimestamp  float64   `json:"lasttimestamp,omitempty" bson:"lastTimestamp"`
+	Count  		   int16     `json:"count,omitempty" bson:"count"`
+	MetricsId      string    `json:"metricsid,omitempty" bson:"metricsId"`
 }
 
 // Error stores an ErrorData and its id
